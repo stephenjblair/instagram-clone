aeba56205bed1c126794054d71bf79da3d4ac4ae
Stephen Blair <stephenjohnblair@outlook.com>
2020-09-24 19:15:42 +0100

    Set up initial file structure and basic layout from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header"> 
+        <img 
+        className="app__headerImage"src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+        alt="instagram-logo"
+        /> 
+      </div>
+      {/* Header */}
+
+      {/* Posts */}
+      {/* Posts */}
+
     </div>
   );
 }
